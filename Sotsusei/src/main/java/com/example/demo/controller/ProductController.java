7bf05b1c3840d9e98a1879f5c77044d9b2f8fdf9
S0514 <S0514@21S0514>
2023-12-15 10:16:57 +0900

    aaa

--- Sotsusei/src/main/java/com/example/demo/controller/ProductController.java
+++ Sotsusei/src/main/java/com/example/demo/controller/ProductController.java
@@ -16,14 +16,25 @@ public class ProductController {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
+	// 新しく追加されたメソッド
+	@RequestMapping(path = "/product/details/{item_number}", method = RequestMethod.GET)
+	public String productDetails(Model model, @PathVariable("item_number") int item_number) {
+		// 商品詳細を取得する処理を追加する（適切なメソッドを使用してデータベースから商品詳細を取得）
+		// 以下はダミーコードで、実際の処理に合わせて変更が必要
+		Map<String, Object> productDetails = jdbcTemplate.queryForMap("select * from merchandise where item_number = ?",
+				item_number);
+		model.addAttribute("productDetails", productDetails);
+		return "product-details";
+	}
 
 	@RequestMapping(path = "/product/purchase/{item_number}", method = RequestMethod.GET)
 	public String purchase(Model model, @PathVariable("item_number") int item_number) {
 
-		Map<String, Object> product= jdbcTemplate.queryForMap("select product_name,price from merchandise where item_number = ?",
+		Map<String, Object> product = jdbcTemplate.queryForMap(
+				"select product_name,price from merchandise where item_number = ?",
 				item_number);
 		model.addAttribute("product", product);
-		return "product-details";
+		return "product";
 	}
 
-	}
+}
