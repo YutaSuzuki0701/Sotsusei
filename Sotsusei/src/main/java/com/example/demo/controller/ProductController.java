cd5c5c164006a19b2eb37ce195de7486233136fc
S0514 <S0514@21S0514>
2023-12-15 12:49:22 +0900

    購入手続き画面表示完了

--- Sotsusei/src/main/java/com/example/demo/controller/ProductController.java
+++ Sotsusei/src/main/java/com/example/demo/controller/ProductController.java
@@ -19,36 +19,32 @@ public class ProductController {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
-	// 新しく追加されたメソッド
 	@RequestMapping(path = "/product/details/{item_number}", method = RequestMethod.GET)
 	public String productDetails(Model model, @PathVariable("item_number") int item_number) {
-		// 商品詳細を取得する処理を追加する（適切なメソッドを使用してデータベースから商品詳細を取得）
-		// 以下はダミーコードで、実際の処理に合わせて変更が必要
 
-		//あとで購入フラグを追加-------0/1の判定
-
-		Map<String, Object> productDetails = jdbcTemplate.queryForMap("select * from merchandise where item_number = ?",
-				item_number);
-		model.addAttribute("productDetails", productDetails);
-		return "product-details";
+	    Map<String, Object> productDetails = jdbcTemplate.queryForMap("select * from merchandise where item_number = ?",
+	            item_number);
+	    model.addAttribute("productDetails", productDetails);
+	    model.addAttribute("item_number", item_number); // item_numberを保存
+	    return "product-details";
 	}
 
 	@RequestMapping(path = "/product/purchase/{item_number}", method = RequestMethod.GET)
-	public String purchase(Model model, @PathVariable("item_number") String item_number) {
-
-		//		Map<String, Object> product= jdbcTemplate.queryForMap("select product_name,price from merchandise where item_number = ?",
-		//				item_number);
-		//		model.addAttribute("product", product);
-		return "purchase";
+	public String purchase(Model model, @PathVariable("item_number") int item_number) {
+	    // item_numberを使用して再度商品詳細を取得
+	    Map<String, Object> productDetails = jdbcTemplate.queryForMap("select * from merchandise where item_number = ?",
+	            item_number);
+	    model.addAttribute("productDetails", productDetails);
+	    return "purchase";
 	}
 
-	//購入
+	////購入
 	@RequestMapping(path = "/product/purchase/{item_number}", method = RequestMethod.POST)
-	public String buy(@PathVariable("item_number") String item_number, String pass, String id, String name,
+	public String buy(@PathVariable("item_number") int item_number, String pass, String id, String name,
 			HttpSession session) {
 
 		System.out.println("check");
-		jdbcTemplate.update("DELETE FROM merchandise where item_number = ?", item_number);
+	//jdbcTemplate.update("DELETE FROM merchandise where item_number = ?", item_number);
 		return "purchase";
 	}
 }
