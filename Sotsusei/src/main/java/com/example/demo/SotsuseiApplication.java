0c190ad41fb6124b65dd7ed2cc77b9765d053412
S0514 <S0514@21S0514>
2024-01-30 13:52:25 +0900

    01/30コミット

--- Sotsusei/src/main/java/com/example/demo/SotsuseiApplication.java
+++ Sotsusei/src/main/java/com/example/demo/SotsuseiApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SotuseiApplication {
+public class SotsuseiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SotuseiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SotsuseiApplication.class, args);
+    }
 }
